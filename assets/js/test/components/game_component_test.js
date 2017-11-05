e8d2a695920e93197dcdd3f966383cb963a48cf4
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2017-11-05 18:46:39 +0100

    mocha tests, needs react setup

---
+++ assets/js/test/components/game_component_test.js
@@ -0,0 +1,12 @@
+import assert from 'assert';
+import gameComponent from '../../components/game_component';
+
+describe('default page', function() {
+
+  it('sees the gameComponent', function () {
+
+    //TODO add enzyme react testing
+    assert.equal(true, true);
+  });
+
+});
--- assets/js/test/pages/default_test.js
+++ assets/js/test/pages/default_test.js
@@ -1,10 +1,9 @@
 import assert from 'assert';
-import chai from 'chai';
 import defaultPage from '../../pages/default';
 
 describe('default page', function() {
 
-  it('Initializes the App', function () {
+  it('Testing the test functionality, this can be removed eventually', function () {
     let page = new defaultPage();
     assert.equal(page.test(), "Hello World");
   });
