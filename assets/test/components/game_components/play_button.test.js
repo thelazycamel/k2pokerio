ebd1b4677d641432b7ed6f77cbf795504fc96848
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2017-12-19 11:59:58 +0100

    add store to play button

--- assets/test/components/game_components/play_button.test.js
+++ assets/test/components/game_components/play_button.test.js
@@ -11,35 +11,71 @@ describe("Play Button", () => {
   beforeEach( () => {
     window.App = MockApp;
     App.t("started"); //fix weird bug where needs a call to App.t before its called in the component
+
+    let initialState = {
+      page: {tabs: {}, links: {}},
+      game: {
+        best_cards: [],
+        cards: ["Kc","2h"],
+        hand_description: "",
+        other_player_status: "ready",
+        player_status: "new",
+        result: {
+          lose_description: "",
+          other_player_cards: [],
+          player_cards: ["Kc", "2d"],
+          status: "in_play",
+          table_cards: [],
+          win_description: "",
+          winning_cards: []
+        },
+        status: "deal",
+        table_cards: []
+      },
+      player: {
+        current_score: 1,
+        username: "stu"
+      },
+      opponent_profile: {
+        blurb: "I am a test user",
+        friend: "na",
+        id: null,
+        image: "bender.png",
+        opponent: "bot",
+        username: "TestingBot"
+      }
+    }
+
+    store = mockStore(initialState);
   });
 
   it("should show 'searching...' whist waiting for opponents", () => {
-    let playButtonComponent = mount(<PlayButtonComponent waiting={true} finished={false} opponent_turn={false} />)
+    let playButtonComponent = mount(<PlayButtonComponent waiting={true} finished={false} opponent_turn={false} store={store} />)
     expect(playButtonComponent.find("#play-button").text()).toEqual("Searching...");
     expect(playButtonComponent.find("#play-button.waiting").length).toEqual(1);
   });
 
   it("should show 'Next Game' when finished", () => {
-    let playButtonComponent = mount(<PlayButtonComponent waiting={false} finished={true} opponent_turn={false} />)
+    let playButtonComponent = mount(<PlayButtonComponent waiting={false} finished={true} opponent_turn={false} store={store} />)
     expect(playButtonComponent.find("#play-button").text()).toEqual("Next Game");
     expect(playButtonComponent.find("#play-button.waiting").length).toEqual(0);
   });
 
   it("should show 'Waiting...' when waiting for opponent to play", () => {
-    let playButtonComponent = mount(<PlayButtonComponent waiting={false} finished={false} opponent_turn={true} />)
+    let playButtonComponent = mount(<PlayButtonComponent waiting={false} finished={false} opponent_turn={true} store={store} />)
     expect(playButtonComponent.find("#play-button").text()).toEqual("Waiting...");
     expect(playButtonComponent.find("#play-button.waiting").length).toEqual(1);
   });
 
   it("should show 'Waiting...' when waiting for opponent in a game with no bots", () => {
     App.settings.bots = "false";
-    let playButtonComponent = mount(<PlayButtonComponent waiting={true} finished={false} opponent_turn={false} />)
+    let playButtonComponent = mount(<PlayButtonComponent waiting={true} finished={false} opponent_turn={false} store={store} />)
     expect(playButtonComponent.find("#play-button").text()).toEqual("Waiting...");
     expect(playButtonComponent.find("#play-button.waiting").length).toEqual(1);
   });
 
   it("should 'Play' when the player can play", () => {
-    let playButtonComponent = mount(<PlayButtonComponent waiting={false} finished={false} opponent_turn={false} />)
+    let playButtonComponent = mount(<PlayButtonComponent waiting={false} finished={false} opponent_turn={false} store={store}/>)
     expect(playButtonComponent.find("#play-button").text()).toEqual("Play");
     expect(playButtonComponent.find("#play-button.waiting").length).toEqual(0);
   });
--- lib/k2poker_io_web/channels/tournament_channel.ex
+++ lib/k2poker_io_web/channels/tournament_channel.ex
@@ -12,16 +12,10 @@ defmodule K2pokerIoWeb.TournamentChannel do
   end
 
   def handle_info({:after_join, tournament_id}, socket) do
-    broadcast! socket, "tournament:refresh_data", %{tournament_id: tournament_id}
-    {:noreply, socket}
-  end
-
-  def handle_in("tournament:refresh_data", %{"tournament_id" => tournament_id}, socket) do
     {tournament_id, _} = Integer.parse(tournament_id)
-    tournament = Repo.get(Tournament, tournament_id)
-    player_id = socket.assigns[:player_id]
     player_count = GetPlayersQuery.count(tournament_id)
-    push socket, "tournament:update",  %{player_count: player_count, tournament_name: tournament.name}
+    tournament_name = Repo.get(Tournament, tournament_id).name
+    broadcast! socket, "tournament:update", %{player_count: player_count, tournament_name: tournament_name}
     {:noreply, socket}
   end
 
