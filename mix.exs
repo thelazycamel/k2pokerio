2813546f8f9ef9d715970ea2d481e71b01e6d483
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2018-11-02 16:27:52 +0100

    App Version 0.5.0 for Beta Release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule K2pokerIo.Mixfile do
 
   def project do
     [app: :k2poker_io,
-     version: "0.0.1",
+     version: "0.5.0",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
