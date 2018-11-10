f1cd8b5514467ee90c42fb275cbcadca4afd7f6f
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2018-11-10 14:51:07 +0100

    Upgrading to 0.5.32

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule K2pokerIo.Mixfile do
 
   def project do
     [app: :k2poker_io,
-     version: "0.5.30",
+     version: "0.5.32",
      elixir: "~> 1.7",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
