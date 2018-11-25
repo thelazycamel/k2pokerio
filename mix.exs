aaeb39b2a224f18e4801925170469f52cd4358f9
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2018-11-25 13:21:30 +0100

    Upgrading version to 0.6.10

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule K2pokerIo.Mixfile do
 
   def project do
     [app: :k2poker_io,
-     version: "0.6.9",
+     version: "0.6.10",
      elixir: "~> 1.7",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
