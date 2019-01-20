6e2845185aaea03add24acd58f27a046ccca6bde
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2019-01-20 19:35:28 +0100

    updating to 0.7.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule K2pokerIo.Mixfile do
 
   def project do
     [app: :k2poker_io,
-     version: "0.7.2",
+     version: "0.7.3",
      elixir: "~> 1.7",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
